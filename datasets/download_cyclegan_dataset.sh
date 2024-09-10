ab53b23d2036a4044535be0cd3292b509811a891
Haule9-2 <76088882+Haule9-2@users.noreply.github.com>
2024-09-10 23:29:22 +0700

    Update download_cyclegan_dataset.sh

--- datasets/download_cyclegan_dataset.sh
+++ datasets/download_cyclegan_dataset.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 FILE=$1
 
-if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter_yosemite" &&  $FILE != "horse2zebra" && $FILE != "monet2photo" && $FILE != "cezanne2photo" && $FILE != "ukiyoe2photo" && $FILE != "vangogh2photo" && $FILE != "maps" && $FILE != "cityscapes" && $FILE != "facades" && $FILE != "iphone2dslr_flower" && $FILE != "mini" && $FILE != "mini_pix2pix" && $FILE != "mini_colorization" ]]; then
-    echo "Available datasets are: apple2orange, summer2winter_yosemite, horse2zebra, monet2photo, cezanne2photo, ukiyoe2photo, vangogh2photo, maps, cityscapes, facades, iphone2dslr_flower, ae_photos"
+# List of valid datasets including CelebA
+if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter_yosemite" && $FILE != "horse2zebra" && $FILE != "monet2photo" && $FILE != "cezanne2photo" && $FILE != "ukiyoe2photo" && $FILE != "vangogh2photo" && $FILE != "maps" && $FILE != "cityscapes" && $FILE != "facades" && $FILE != "iphone2dslr_flower" && $FILE != "mini" && $FILE != "mini_pix2pix" && $FILE != "mini_colorization" && $FILE != "celebA" ]]; then
+    echo "Available datasets are: apple2orange, summer2winter_yosemite, horse2zebra, monet2photo, cezanne2photo, ukiyoe2photo, vangogh2photo, maps, cityscapes, facades, iphone2dslr_flower, ae_photos, celebA"
     exit 1
 fi
 
@@ -11,11 +14,19 @@ if [[ $FILE == "cityscapes" ]]; then
     exit 1
 fi
 
+if [[ $FILE == "celebA" ]]; then
+    echo "Downloading CelebA dataset from Kaggle..."
+    kaggle datasets download -d jessicali9530/celeba-dataset -p ./datasets/celebA/
+    unzip ./datasets/celebA/celeba-dataset.zip -d ./datasets/celebA/
+    rm ./datasets/celebA/celeba-dataset.zip
+    exit 0
+fi
+
 echo "Specified [$FILE]"
 URL=http://efrosgans.eecs.berkeley.edu/cyclegan/datasets/$FILE.zip
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
-mkdir $TARGET_DIR
+mkdir -p $TARGET_DIR
 unzip $ZIP_FILE -d ./datasets/
 rm $ZIP_FILE
