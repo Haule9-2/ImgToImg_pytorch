34b67fc1a9ca9d3ce63422b444acadb3d188e869
Haule9-2 <76088882+Haule9-2@users.noreply.github.com>
2024-09-10 23:31:14 +0700

    Update download_cyclegan_dataset.sh

--- datasets/download_cyclegan_dataset.sh
+++ datasets/download_cyclegan_dataset.sh
@@ -26,7 +26,7 @@ echo "Specified [$FILE]"
 URL=http://efrosgans.eecs.berkeley.edu/cyclegan/datasets/$FILE.zip
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
-wget -N $URL -O $ZIP_FILE
 mkdir -p $TARGET_DIR
+wget -N $URL -O $ZIP_FILE
 unzip $ZIP_FILE -d ./datasets/
 rm $ZIP_FILE
